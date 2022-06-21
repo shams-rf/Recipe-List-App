7833eec0f74b9e986b4181261cf6558eaed94c72
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-06-21 10:17:47 +0100

    Initial Commit

---
+++ Shared/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Shared
+//
+//  Created by Shamsuddin Refaei on 21/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
