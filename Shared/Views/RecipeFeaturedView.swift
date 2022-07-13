2543ab88e146da5c54eeb6d80c67d1c9b215bbf4
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-07-13 18:11:37 +0100

    Create RecipeFeaturedView

---
+++ Shared/Views/RecipeFeaturedView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeFeaturedView.swift
+//  Recipe-List-App
+//
+//  Created by Shamsuddin Refaei on 13/07/2022.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+    }
+}
