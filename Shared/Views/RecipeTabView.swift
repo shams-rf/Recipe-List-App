c1bca979da63825e108da849602be0e08ea83140
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-07-13 13:37:43 +0100

    Add RecipeTabView as starting point for app

---
+++ Shared/Views/RecipeTabView.swift
@@ -0,0 +1,38 @@
+//
+//  RecipeTabView.swift
+//  Recipe-List-App
+//
+//  Created by Shamsuddin Refaei on 13/07/2022.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        TabView {
+            
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            
+            ContentView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
